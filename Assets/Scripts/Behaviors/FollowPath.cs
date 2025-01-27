5cadc944d7b3d9ee5820b6a45d00926eede6f111
David Close <114041624+DavidClose0@users.noreply.github.com>
2025-01-26 21:21:58 -0700

    Initial commit

---
+++ Assets/Scripts/Behaviors/FollowPath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : Seek
+{
+    // list of waypoints
+    public GameObject[] path;
+    public int pathIndex = 0;
+    public float targetThreshold = .1f;
+
+    public override SteeringOutput getSteering()
+    {
+        // initialize target to first waypoint
+        if (target == null)
+        {
+            target = path[pathIndex];
+        }
+
+        // increment path index when waypoint is reached
+        if ((target.transform.position - character.transform.position).magnitude < targetThreshold)
+        {
+            pathIndex = (pathIndex + 1) % path.Length;
+            target = path[pathIndex];
+        }
+
+        return base.getSteering();
+    }
+
+}
